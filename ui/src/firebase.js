1445483b32bd9d169aa0f92fda582e5f355c0529
Jack Zhang <zhangmengyu10@gmail.com>
2021-02-20 15:04:57 -0800

    Fix indentation.

--- ui/src/firebase.js
+++ ui/src/firebase.js
@@ -13,13 +13,13 @@ import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDDnGW8SPLYZ_TgaYVXxkiX4rp6YnYbzEE",
-    authDomain: "wewish.firebaseapp.com",
-    projectId: "wewish",
-    storageBucket: "wewish.appspot.com",
-    messagingSenderId: "371870672394",
-    appId: "1:371870672394:web:a1609b47563393f5f7e1f3",
-    measurementId: "G-D7RQ2S0B8V"
+  apiKey: "AIzaSyDDnGW8SPLYZ_TgaYVXxkiX4rp6YnYbzEE",
+  authDomain: "wewish.firebaseapp.com",
+  projectId: "wewish",
+  storageBucket: "wewish.appspot.com",
+  messagingSenderId: "371870672394",
+  appId: "1:371870672394:web:a1609b47563393f5f7e1f3",
+  measurementId: "G-D7RQ2S0B8V"
 };
 
 // Initialize Firebase
@@ -29,7 +29,7 @@ const db = firebase.firestore();
 var user = firebase.auth().currentUser;
 
 const addWish = (name) => {
-    
+  
 }
 
 export default fb;
