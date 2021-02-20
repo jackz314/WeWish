e5d10bfb3f18761edf19d60ef91c40451834a40b
Yingjie Shen <shen92@wisc.edu>
2021-02-20 16:55:55 -0600

    fix

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      hello world!
+      <Text>hello world!</Text>
     </View>
   );
 }
