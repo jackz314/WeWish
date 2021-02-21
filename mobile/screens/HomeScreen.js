f46293aa1b0445844b82ce5b9a88cc71143e1a7c
Yingjie Shen <shen92@wisc.edu>
2021-02-20 21:19:07 -0600

    Update HomeScreen.js

--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Modal, View, Text, TextInput, StyleSheet} from 'react-native';
+import {Button, Modal, View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
@@ -8,6 +8,7 @@ function HomeScreen() {
   const [open, setOpen] = useState(false);
   const [wishName, setWishName] = useState("New Wish");
   const [wishLevel, setWishLevel] = useState("easy");
+  const [wishComment, setWishComment] = useState("Tell us more about your wish");
 
    const onAddWishButtonPress = () => {
     setOpen(true);
@@ -21,13 +22,25 @@ function HomeScreen() {
     setOpen(false);
    }
 
-   const onTextInputChange = (text) => {
+   const onWishNameTextInputChange = (text) => {
      if(text.length !== 0){
       setWishName(text);
      }else{
       setWishName("New Wish")
      }
    }
+
+   const onLevelTagPress = (level) => {
+     setWishLevel(level);
+   }
+
+   const onWishCommentTextInputChange = (text) => {
+    if(text.length !== 0){
+      setWishComment(text);
+    }else{
+      setWishComment("New Wish")
+    }
+  }
 
 
 
@@ -43,29 +56,88 @@ function HomeScreen() {
             <Text style={styles.titleText}>Make a New Wish</Text>
           </View>
           <View style={{flex: 7, justifyContent: 'center', alignItems: 'center',}}>
-            <Text style={{marginBottom: 10}}>What's your wish?</Text>
+            <Text style={{marginBottom: 10, fontSize: 20}}>
+              What's your wish?
+              </Text>
             <TextInput
-              style={{ height: 40, 
+              style={{ 
+                height: 40, 
                 width: '80%', 
                 borderColor: 'gray', 
                 borderWidth: 1, 
                 marginBottom: 10, 
+                fontSize: 16,
                 color: wishName === 'Wish Name' ?  '#999999' : '#0e0e0e' }}
-              onChangeText={text => onTextInputChange(text)}
+              onChangeText={text => onWishNameTextInputChange(text)}
               value={wishName}
               clearTextOnFocus={true}
             />
-            <Text>What's the difficulty to accomplish?</Text>
-            <View style={{flexDirection: "row", justifyContent: 'center'}}>
-              <View id="easy">
-                <Text>Easy</Text>
-              </View>
-              <View id="medium">
-                <Text>Medium</Text>
-              </View>
-              <View id="hard">
-                <Text>Hard</Text>
-              </View>
+            <Text style={{marginBottom: 10, fontSize: 20}}>
+              Do you have any comments?
+            </Text>
+            <TextInput
+              multiline
+              numberOfLines={3}
+              style={{ 
+                height: 120, 
+                width: '80%', 
+                borderColor: 'gray', 
+                borderWidth: 1, 
+                marginBottom: 10, 
+                fontSize: 16,
+                color: wishName === 'Wish Name' ?  '#999999' : '#0e0e0e' }}
+              onChangeText={text => onWishCommentTextInputChange(text)}
+              value={wishComment}
+              clearTextOnFocus={true}
+            />
+            <Text style={{marginBottom: 10, fontSize: 20}}>
+              What's the difficulty to accomplish?
+            </Text>
+            <View style={{flexDirection: "row", justifyContent: 'center', marginBottom: 10}}>
+              <TouchableOpacity 
+                style={{
+                  borderRadius: 8, 
+                  backgroundColor: wishLevel === 'easy' ? 'rgb(32,171,51)' : 'rgba(32,171,51,0.25)',
+                  paddingTop: 2,
+                  paddingRight: 6,
+                  paddingBottom: 2,
+                  paddingLeft: 6,
+                  marginLeft: 4,
+                  marginRight: 4
+                }}
+                onPress={() => onLevelTagPress('easy')}
+              >
+                <Text style={{color: "white", fontSize: 15}}>Easy</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  borderRadius: 8, 
+                  backgroundColor: wishLevel === 'medium' ? 'rgb(247,157,22)' : 'rgba(247,157,22,0.25)',
+                  paddingTop: 2,
+                  paddingRight: 6,
+                  paddingBottom: 2,
+                  paddingLeft: 6,
+                  marginLeft: 4,
+                  marginRight: 4
+                }} 
+                onPress={() => onLevelTagPress('medium')}
+                >
+                <Text style={{color: "white", fontSize: 15}}>Medium</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  borderRadius: 8, 
+                  backgroundColor: wishLevel === 'hard' ? 'rgb(255,10,10)' : 'rgba(255,10,10,0.25)',
+                  paddingTop: 2,
+                  paddingRight: 6,
+                  paddingBottom: 2,
+                  paddingLeft: 6,
+                  marginLeft: 4,
+                  marginRight: 4
+                }} 
+                onPress={() => onLevelTagPress('hard')}>
+                <Text style={{color: "white", fontSize: 15}}>Hard</Text>
+              </TouchableOpacity>
             </View>
           </View>
           <View style={{flex: 2, flexDirection: "row", justifyContent: 'center'}}>
