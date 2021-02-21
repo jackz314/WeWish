a1bf11bac5de85180ba45c8802405f524868dc4f
Shanmukh Swaroop <shanmukhs99@gmail.com>
2021-02-20 22:51:45 -0500

    Sidebar functionality

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Dashboard from './components/Dashboard'
+import Wishlist from './components/Wishlist'
+import Groups from './components/Groups'
+import Explore from './components/Explore'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Router>
+          <Sidebar />
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/wishlist' component={Wishlist} />
+          <Route path='/group' component={Groups} />
+          <Route path='/explore' component={Explore} />
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
